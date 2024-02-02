ba55da9848e2b2b0372ee3d1d9056d45b0695ff5
RinEster <kliuevairina05@gmail.com>
2024-02-02 22:49:19 +0400

    Реализован список возможных тестов по курсам

--- ProjectCoder/View/Tests.xaml.cs
+++ ProjectCoder/View/Tests.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,25 @@ namespace ProjectCoder.View
         public Tests()
         {
             InitializeComponent();
+            TestList();
         }
+
+        /// <summary>
+        /// добавление списка тестов
+        /// </summary>
+        public void TestList()
+        {
+            SqlConnection con = new SqlConnection(Courses.ConnStr);
+            SqlCommand command = new SqlCommand("Select Distinct  TopicDirectory.[Name],BankQuestions.TopicID From TopicDirectory inner join  " +
+                "BankQuestions on BankQuestions.TopicID = TopicDirectory.ID", con);
+            con.Open(); //Открываем соединение
+            SqlDataReader read = command.ExecuteReader();
+            while (read.Read())
+            {
+                test.Items.Add(read.GetValue(0).ToString());
+            }
+            con.Close();
+        }
+      
     }
 }
