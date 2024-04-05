986c02d33c79972a4fc280aff96a6dc58ee3e257
RinEster <kliuevairina05@gmail.com>
2024-04-06 00:31:06 +0400

    Частично реализованы задачи для редактора кода

--- ProjectCoder/View/CodeEditor.xaml.cs
+++ ProjectCoder/View/CodeEditor.xaml.cs
@@ -16,35 +16,113 @@ namespace ProjectCoder.View
             InitializeComponent();
             string filePath = "TXTFiles\\NewProject.txt"; 
             string fileContent = File.ReadAllText(filePath); 
-            newProject1.Document.Blocks.Clear(); 
-            newProject1.AppendText(fileContent);
-            newProject2.Document.Blocks.Clear();
-            newProject2.AppendText(fileContent);
-            newProject3.Document.Blocks.Clear();
-            newProject3.AppendText(fileContent);
+            newProject.Document.Blocks.Clear(); 
+            newProject.AppendText(fileContent);
         }
 
         private void check_Click(object sender, RoutedEventArgs e)
         {
-            checkCode(newProject1.Document.ContentStart, newProject1.Document.ContentEnd);
+            switch (task)
+            {
+                case "1":
+
+                    string task1 = "TXTFiles\\task1.txt";
+                    checkCode(newProject.Document.ContentStart, newProject.Document.ContentEnd, task1);
+                    break;
+                case "2":
+                    string task2 = "TXTFiles\\task2.txt";
+                    checkCode(newProject.Document.ContentStart, newProject.Document.ContentEnd, task2); 
+                    break;
+                case "3":
+                    string task3 = "TXTFiles\\task3.txt";
+                    checkCode(newProject.Document.ContentStart, newProject.Document.ContentEnd, task3); 
+                    break;
+                case "4":
+                    string task4 = "TXTFiles\\task4.txt";
+                    checkCode(newProject.Document.ContentStart, newProject.Document.ContentEnd, task4);
+                    break;
+                case "5":
+                    string task5 = "TXTFiles\\task5.txt";
+                    checkCode(newProject.Document.ContentStart, newProject.Document.ContentEnd, task5);
+                    break;
+                case "6":
+                    string task6 = "TXTFiles\\task6.txt";
+                    checkCode(newProject.Document.ContentStart, newProject.Document.ContentEnd, task6);
+                    break;
+                case "7":
+                    string task7 = "TXTFiles\\task7.txt";
+                    checkCode(newProject.Document.ContentStart, newProject.Document.ContentEnd, task7);
+                    break;
+
+            }
+
+
            
         }
 
-        void checkCode(TextPointer projectStart, TextPointer projectEnd)
-        {
-            string filePath = "TXTFiles\\NewProject.txt";
+        void checkCode(TextPointer projectStart, TextPointer projectEnd,string filePath)
+        {            
             string fileContent = File.ReadAllText(filePath);
             TextRange textRange = new TextRange(projectStart, projectEnd);
             string richTextBoxContent = textRange.Text;
 
             if (fileContent == richTextBoxContent)
             {
-                MessageBox.Show("Содержимое файла совпадает с текстом в RichTextBox.");
+                MessageBox.Show("Верно");
             }
-            else
+            
+        }
+      
+
+        private void checkTask_Checked(object sender, RoutedEventArgs e)
+        { 
+            taskListView.Visibility = Visibility.Visible;          
+        }
+
+        private void checkTask_Unchecked(object sender, RoutedEventArgs e)
+        {
+            taskListView.Visibility = Visibility.Hidden;
+            taskGrid.Height = 0;
+            richGrid.Height = 430;
+        }
+
+        public string task;
+
+        
+        private void taskListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selected = (sender as ListView).SelectedItem;
+            task = selected.ToString();
+            int firstSpaceIndex = task.IndexOf(' '); // Индекс первого пробела
+            int secondSpaceIndex = task.IndexOf(' ', firstSpaceIndex + 1); // Индекс второго пробела
+            task = task.Substring(secondSpaceIndex + 1);
+            switch (task)
             {
-                MessageBox.Show("Содержимое файла не совпадает с текстом в RichTextBox.");
+                case "1":
+                    taskTextBlock.Text = "Напишите консольную программу, в которую пользователь вводит с клавиатуры два числа. А программа должна выводить на консоль сумму двух введенных чисел.";
+                    break;
+                case "2":
+                    taskTextBlock.Text = "Напишите консольную программу, в которую пользователь вводит с клавиатуры два числа. А программа должна выводить на консоль большее число (в программе использовать if-else)";
+                    break;
+                case "3":
+                    taskTextBlock.Text = "Напишите консольную программу, в которую пользователь вводит с клавиатуры два числа - a и b. А программа должна выводить на консоль все числа начиная от a, и заканчивая b (в программе использовать for)";
+                    break;
+                case "4":
+                    taskTextBlock.Text = "Напишите программу, которая запрашивает число у пользователя и сообщает, является ли оно четным или нечетным.";
+                    break;
+                case "5":
+                    taskTextBlock.Text = "Напишите программу, которая запрашивает длину и ширину прямоугольника, а затем выводит его площадь.";
+                    break;
+                case "6":
+                    taskTextBlock.Text = "Напишите программу, которая проверяет, является ли введенное пользователем число простым.";
+                    break;
+                case "7":
+                    taskTextBlock.Text = "Напишите программу, которая сортирует массив целых чисел { 1, 12, 31, 5, 23 } по возрастанию ";
+                    break;               
+
             }
+            taskGrid.Height = 60;
+            richGrid.Height = 370;            
         }
     }
 }
